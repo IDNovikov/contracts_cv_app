91822d9445495e0f1943df51b1c23e39d38c24f4
Ilya <affective.neurosis@gmail.com>
2026-04-22 10:06:19 +0300

    ...

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export * as AuthProto from './gen/auth';
 export * as UserProto from './gen/user';
+export * as ChatProto from './gen/chat';
 
 export * from './contracts/exchanges/mail.exchange';
 export * from './contracts/queues/mail/request-type';
